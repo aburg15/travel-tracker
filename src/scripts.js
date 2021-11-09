5b821bc4bffbf912affeeb74828a8b81eecc37b3
Adam Burgess <aburg1515@gmail.com>
2021-11-09 13:09:30 -0700

    Test connection

--- src/scripts.js
+++ src/scripts.js
@@ -8,4 +8,4 @@ import './css/base.scss';
 import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+console.log('This is the JavaScript entry file - your code begins here!!');
